49b463577780f8a976594f519627983e4e615a55
Julian Osmer <julian.osmer@outlook.de>
2019-04-03 18:09:27 +0200

    changes

---
+++ sql/create_radiation.sql
@@ -0,0 +1,22 @@
+-- Table: public.radiation
+
+-- DROP TABLE public.radiation;
+
+CREATE TABLE public.radiation
+(
+    typ character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    date integer NOT NULL,
+    x_min integer NOT NULL,
+    x_max integer NOT NULL,
+    y_min integer NOT NULL,
+    y_max integer NOT NULL,
+    value real NOT NULL,
+    CONSTRAINT radiation_pkey PRIMARY KEY (typ, date, x_min, x_max, y_min, y_max)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.radiation
+    OWNER to postgres;
--- src/main/java/org/josmer/controller/RadiationController.java
+++ src/main/java/org/josmer/controller/RadiationController.java
@@ -6,8 +6,6 @@ import org.josmer.security.Key;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/radiation")
 public final class RadiationController {
@@ -15,13 +13,12 @@ public final class RadiationController {
     IRadiationRepository radiationRepository;
 
     @ResponseBody
-    @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public Radiation get(@CookieValue("key") final String key, @RequestParam(name = "id") long id) {
+    @RequestMapping(value = "/find", method = RequestMethod.GET)
+    public Radiation find(@CookieValue("key") final String key, @RequestParam(name = "id") long id) {
         if (!Key.check(key)) {
             return null;
         }
-        Optional<Radiation> optionalRadiation = radiationRepository.get(id);
-        return optionalRadiation.isPresent() ? optionalRadiation.get() : new Radiation();
+        return new Radiation();
     }
 
 }
