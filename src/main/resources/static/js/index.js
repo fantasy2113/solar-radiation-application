6f5ccd12f4010f96b91c443cf76a583c99d723fc
fantasy2113 <49161667+fantasy2113@users.noreply.github.com>
2019-04-03 12:10:49 +0200

    changes

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function () {
                 request.setRequestHeader('login', $('input[name=username]').val());
                 request.setRequestHeader('password', $('input[name=password]').val());
             },
-            url: M_originPath + 'key',
+            url: getPath() + 'key',
             method: 'GET',
             success: function (data) {
                 document.cookie = 'key=' + data + ';';
