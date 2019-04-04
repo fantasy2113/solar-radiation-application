092edd92f08e39cd55592c2916be15dedaa2c9f5
fantasy2113 <49161667+fantasy2113@users.noreply.github.com>
2019-04-04 11:39:12 +0200

    changes

--- src/main/java/org/josmer/controller/ApplicationController.java
+++ src/main/java/org/josmer/controller/ApplicationController.java
@@ -5,7 +5,6 @@ import org.josmer.security.Key;
 import org.josmer.utils.Toolbox;
 import org.springframework.web.bind.annotation.*;
 import java.io.File;
-import java.io.IOException;
 
 @RestController
 public class ApplicationController {
@@ -16,10 +15,9 @@ public class ApplicationController {
             return Toolbox.readFile("src/main/resources/static/html/accessDenied.html");
         }
         
-        try {
+      
          File file = new File("src/main/resources/static/test.txt");
-        } catch(IOException ioe) {;
-        }
+  
         
         return Toolbox.readFile("src/main/resources/static/html/app.html");
     }
