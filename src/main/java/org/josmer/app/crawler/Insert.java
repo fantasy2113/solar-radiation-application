7d914ea82ea8f9081ce8a1a481f84b1c03280a2c
Julian Osmer <julian.osmer@outlook.de>
2019-04-05 21:21:47 +0200

    add

--- src/main/java/org/josmer/app/crawler/Insert.java
+++ src/main/java/org/josmer/app/crawler/Insert.java
@@ -5,10 +5,8 @@ import org.josmer.app.repository.RadiationRepository;
 
 public class Insert {
 
-    private static String dbUrl = "postgres://ntdzcywnkjukss:cd01495e80208c5d6961118ef26bd5e0edf15e3cfa906f33ba018a85940447cf@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/d81ptonl3ncqr9";
-
     public static void main(String[] args) {
-        if (!new RadiationRepository(dbUrl).isConnected()) {
+        if (!new RadiationRepository().isConnected()) {
             System.err.println("no db connection");
             return;
         }
@@ -22,7 +20,7 @@ public class Insert {
                 RadiationCrawler radiationCrawler = new RadiationCrawler(month, year, RadiationTypes.GLOBAL);
                 radiationCrawler.download();
                 radiationCrawler.unzip();
-                radiationCrawler.insert(dbUrl);
+                radiationCrawler.insert();
                 radiationCrawler.delete();
                 System.out.println();
             }
