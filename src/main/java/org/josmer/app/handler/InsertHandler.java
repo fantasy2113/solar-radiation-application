f6550626a71190fb38d8c5d190a50f29e9f67258
Julian Osmer <julian.osmer@outlook.de>
2019-04-05 21:23:01 +0200

    changes

--- src/main/java/org/josmer/app/handler/InsertHandler.java
+++ src/main/java/org/josmer/app/handler/InsertHandler.java
@@ -8,13 +8,11 @@ public class InsertHandler implements Runnable {
     public void run() {
         for (int year = 2018; year < 2019; year++) {
             for (int month = 1; month < 13; month++) {
-                System.out.println(">>> Month: " + month + ", Year: " + year);
                 RadiationCrawler radiationCrawler = new RadiationCrawler(month, year, RadiationTypes.GLOBAL);
                 radiationCrawler.download();
                 radiationCrawler.unzip();
-                radiationCrawler.insert("");
+                radiationCrawler.insert();
                 radiationCrawler.delete();
-                System.out.println();
             }
         }
     }
