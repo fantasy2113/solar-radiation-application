cb5a0080f486618707b57726bac7cafa714496c9
Julian Osmer <julian.osmer@outlook.de>
2019-04-18 23:34:11 +0200

    changes

--- src/main/java/de/josmer/application/repositories/ExportRadiRepository.java
+++ src/main/java/de/josmer/application/repositories/ExportRadiRepository.java
@@ -14,9 +14,35 @@ public class ExportRadiRepository extends AExportRepository<ExportRadi, Radiatio
     @Override
     public List<ExportRadi> getAll(final List<Radiation> radiations, final double lon, final double lat) {
         List<ExportRadi> exports = new LinkedList<>();
-        for (Radiation radiation : radiations) {
-            exports.add(mapToExport(lon, lat, radiation));
+
+        try {
+            double eGlobHorSum = 0.0;
+
+            for (Radiation radiation : radiations) {
+                exports.add(mapToExport(lon, lat, radiation));
+                eGlobHorSum += radiation.getRadiationValue();
+                if (String.valueOf(radiation.getRadiationDate()).endsWith("12")) {
+                    ExportRadi export = new ExportRadi();
+                    export.setDate("Summe " + String.valueOf(radiation.getRadiationDate()).substring(0, 4));
+                    export.setLat("");
+                    export.setLon("");
+                    export.setType("");
+                    export.setValue(Double.valueOf(round(eGlobHorSum, 2)));
+                    export.setUnit("kWh/m2");
+                    export.setDim("1 km2");
+                    export.setSource("DWD CDC");
+
+                    exports.add(export);
+
+                    eGlobHorSum = 0.0;
+                }
+            }
+
+
+        } catch (Exception e) {
+            LOGGER.info(e.getMessage());
         }
+
         return exports;
     }
 
