c8904915d05fb65eeddfd69ad040c30ea673e728
Julian Osmer <julian.osmer@outlook.de>
2019-04-18 21:27:38 +0200

    changes

--- src/main/java/de/josmer/application/repositories/AExportRepository.java
+++ src/main/java/de/josmer/application/repositories/AExportRepository.java
@@ -1,27 +1,31 @@
 package de.josmer.application.repositories;
 
 import org.apache.commons.math3.util.Precision;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Locale;
 
 abstract class AExportRepository<R, I> {
+    static final Logger LOGGER = LoggerFactory.getLogger(AExportRepository.class.getName());
 
-    protected String round(double geo) {
+    String round(double geo) {
         return String.format(Locale.ENGLISH, "%.3f", Precision.round(geo, 3));
     }
 
-    protected double getValue(double val) {
+    double getValue(double val) {
         return Double.parseDouble(String.format(Locale.ENGLISH, "%.2f", val));
     }
 
-    protected String parseDate(final int date) {
+    String parseDate(final int date) {
         return getDate(String.valueOf(date));
     }
 
-    protected String getDate(final String date) {
+    String getDate(final String date) {
         try {
             return date.substring(0, 4) + "-" + date.substring(4);
         } catch (Exception e) {
+            LOGGER.info(e.getMessage());
             return "undefined";
         }
     }
