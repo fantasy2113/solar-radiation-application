28ab1ab81c847e93ff966fe15bfde8de7a71a3c6
Julian Osmer <julian.osmer@outlook.de>
2019-05-17 15:44:45 +0200

    changes

--- src/main/java/de/josmer/application/controller/ViewController.java
+++ src/main/java/de/josmer/application/controller/ViewController.java
@@ -2,7 +2,7 @@ package de.josmer.application.controller;
 
 import de.josmer.application.controller.security.JwtToken;
 import de.josmer.application.library.interfaces.IUserRepository;
-import de.josmer.application.library.utils.Toolbox;
+import de.josmer.application.library.utils.FileReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,13 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class ViewController extends Controller {
-    private static final String LOGIN_HTML = Toolbox.readFile("src/main/resources/static/html/login.html");
-    private static final String IRR_HTML = Toolbox.readFile("src/main/resources/static/html/irr.html");
-    private static final String RAD_HTML = Toolbox.readFile("src/main/resources/static/html/rad.html");
+    private final String loginHtml;
+    private final String irrHtml;
+    private final String radHtml;
 
     @Autowired
-    public ViewController(IUserRepository userRep, JwtToken jwtToken) {
+    public ViewController(IUserRepository userRep, JwtToken jwtToken, FileReader fileReader) {
         super(userRep, jwtToken);
+        this.loginHtml = fileReader.asString("src/main/resources/static/html/login.html");
+        this.irrHtml = fileReader.asString("src/main/resources/static/html/irr.html");
+        this.radHtml = fileReader.asString("src/main/resources/static/html/rad.html");
     }
 
     @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
@@ -38,14 +41,14 @@ public class ViewController extends Controller {
             }
             if (isAccess(token)) {
                 if (app.equals("irr")) {
-                    return IRR_HTML;
+                    return irrHtml;
                 } else {
-                    return RAD_HTML;
+                    return radHtml;
                 }
             }
         } catch (Exception e) {
             LOGGER.info(e.getMessage());
         }
-        return LOGIN_HTML;
+        return loginHtml;
     }
 }
