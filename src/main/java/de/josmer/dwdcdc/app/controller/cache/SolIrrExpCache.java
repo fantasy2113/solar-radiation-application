af5406bab90bb2b6fff5bd8864016d3f9c123404
Julian Osmer <julian.osmer@outlook.de>
2019-06-08 19:22:39 +0200

    changes

--- src/main/java/de/josmer/dwdcdc/app/controller/cache/SolIrrExpCache.java
+++ src/main/java/de/josmer/dwdcdc/app/controller/cache/SolIrrExpCache.java
@@ -3,8 +3,6 @@ package de.josmer.dwdcdc.app.controller.cache;
 import de.josmer.dwdcdc.app.controller.requests.IrrRequest;
 import de.josmer.dwdcdc.app.entities.SolIrrExp;
 import de.josmer.dwdcdc.app.interfaces.ISolIrrExpCache;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.LinkedList;
@@ -13,8 +11,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class SolIrrExpCache implements ISolIrrExpCache {
-    protected static final Logger LOGGER = LoggerFactory.getLogger(SolIrrExpCache.class.getName());
-    private static final int LIMIT = 1000;
+    private static final int LIMIT = 10000;
     private final ConcurrentHashMap<String, LinkedList<SolIrrExp>> computedSolIrrExps;
 
     public SolIrrExpCache() {
@@ -23,30 +20,18 @@ public class SolIrrExpCache implements ISolIrrExpCache {
 
     @Override
     public void add(final IrrRequest irrRequest, final LinkedList<SolIrrExp> solIrrExps) {
-        try {
-            clear();
-            final String key = irrRequest.toString();
-            computedSolIrrExps.putIfAbsent(key, solIrrExps);
-            LOGGER.info("create cache for: " + key);
-        } catch (Exception e) {
-            LOGGER.info(e.getMessage());
-        }
+        cleaning();
+        computedSolIrrExps.putIfAbsent(irrRequest.toString(), solIrrExps);
     }
 
     @Override
     public Optional<LinkedList<SolIrrExp>> get(final IrrRequest irrRequest) {
-        try {
-            return Optional.ofNullable(computedSolIrrExps.get(irrRequest.toString()));
-        } catch (Exception e) {
-            LOGGER.info(e.getMessage());
-        }
-        return Optional.empty();
+        return Optional.ofNullable(computedSolIrrExps.get(irrRequest.toString()));
     }
 
-    private void clear() {
+    private void cleaning() {
         if (computedSolIrrExps.size() > LIMIT) {
-            computedSolIrrExps.clear();
-            LOGGER.info("clear");
+            computedSolIrrExps.remove(computedSolIrrExps.keys().nextElement());
         }
     }
 }
