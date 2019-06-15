9c801b019fd41b2a5d77e760486bb16a78f6cd7d
Julian Osmer <julian.osmer@outlook.de>
2019-06-15 14:26:29 +0200

    changes

--- src/main/java/de/josmer/dwdcdc/app/App.java
+++ src/main/java/de/josmer/dwdcdc/app/App.java
@@ -12,11 +12,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 
-@Configuration
 @SpringBootApplication
 public class App {
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class.getName());
