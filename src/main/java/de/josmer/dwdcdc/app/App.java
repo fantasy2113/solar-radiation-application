d19cb52276dba43a659e516b29a1eb80a42d5f70
Julian Osmer <julian.osmer@outlook.de>
2019-06-18 17:07:28 +0200

    changes

--- src/main/java/de/josmer/dwdcdc/app/App.java
+++ src/main/java/de/josmer/dwdcdc/app/App.java
@@ -4,6 +4,7 @@ import de.josmer.dwdcdc.app.repositories.SolRadRepository;
 import de.josmer.dwdcdc.app.spring.Beans;
 import de.josmer.dwdcdc.app.spring.Context;
 import de.josmer.dwdcdc.app.utils.FileReader;
+import de.josmer.dwdcdc.utils.handler.SolRadInsertAllHandler;
 import de.josmer.dwdcdc.utils.handler.SolRadInsertAtMidnightHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,36 +26,59 @@ public class App {
     }
 
     @Bean
-    public void startInsertHandler() {
-        if (App.isTest) {
-            return;
-        }
-        System.getenv("INSERT_ALL").equals("parallel")
-        System.getenv("INSERT_ALL") == null
-        new SolRadInsertAtMidnightHandler(
-                Context.getCrawler(Beans.CRAWLER_GLOBAL),
-                Context.getBean(SolRadRepository.class),
-                Context.getBean(FileReader.class)
-        ).start();
-
-        new SolRadInsertAtMidnightHandler(
-                Context.getCrawler(Beans.CRAWLER_DIRECT),
-                Context.getBean(SolRadRepository.class),
-                Context.getBean(FileReader.class)
-        ).start();
-
-        new SolRadInsertAtMidnightHandler(
-                Context.getCrawler(Beans.CRAWLER_DIFFUSE),
-                Context.getBean(SolRadRepository.class),
-                Context.getBean(FileReader.class)
-        ).start();
-    }
-
-    @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
             LOGGER.info("Let's inspect the beans provided by Spring Boot:");
             Arrays.stream(ctx.getBeanDefinitionNames()).sorted().forEachOrdered(LOGGER::info);
         };
+    }
+
+    @Bean
+    public void startInsertHandler() {
+        if (App.isTest) {
+            return;
+        }
+        if (isInsertAll()) {
+            startInsertAllHandler();
+        } else {
+            startInsertAtMidnight();
+        }
+    }
+
+    private void startInsertAtMidnight() {
+        new SolRadInsertAtMidnightHandler(Context.getCrawler(Beans.CRAWLER_GLOBAL),
+                Context.getBean(SolRadRepository.class),
+                Context.getBean(FileReader.class)).start();
+
+        new SolRadInsertAtMidnightHandler(Context.getCrawler(Beans.CRAWLER_DIRECT),
+                Context.getBean(SolRadRepository.class),
+                Context.getBean(FileReader.class)).start();
+
+        new SolRadInsertAtMidnightHandler(Context.getCrawler(Beans.CRAWLER_DIFFUSE),
+                Context.getBean(SolRadRepository.class),
+                Context.getBean(FileReader.class)).start();
+    }
+
+    private void startInsertAllHandler() {
+        boolean parallel = isInsertAllParallel();
+        new SolRadInsertAllHandler(Context.getCrawler(Beans.CRAWLER_GLOBAL),
+                Context.getBean(SolRadRepository.class),
+                Context.getBean(FileReader.class), parallel).start();
+
+        new SolRadInsertAllHandler(Context.getCrawler(Beans.CRAWLER_DIRECT),
+                Context.getBean(SolRadRepository.class),
+                Context.getBean(FileReader.class), parallel).start();
+
+        new SolRadInsertAllHandler(Context.getCrawler(Beans.CRAWLER_DIFFUSE),
+                Context.getBean(SolRadRepository.class),
+                Context.getBean(FileReader.class), parallel).start();
+    }
+
+    private boolean isInsertAllParallel() {
+        return isInsertAll() && System.getenv("INSERT_ALL").equals("parallel");
+    }
+
+    private boolean isInsertAll() {
+        return System.getenv("INSERT_ALL") != null;
     }
 }
