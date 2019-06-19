f71f19be07e7982f050b323c97db9251d9322235
Julian Osmer <julian.osmer@outlook.de>
2019-06-19 14:53:46 +0200

    changes

--- src/main/java/de/josmer/dwdcdc/app/interfaces/IDbCacheRepository.java
+++ src/main/java/de/josmer/dwdcdc/app/interfaces/IDbCacheRepository.java
@@ -1,12 +1,10 @@
 package de.josmer.dwdcdc.app.interfaces;
 
-import de.josmer.dwdcdc.app.entities.DbCache;
-
 import java.util.Optional;
 
 public interface IDbCacheRepository<T extends IJsonb> {
 
-    Optional<DbCache> find(String key);
+    Optional<T> find(String key);
 
     void save(T dbCache);
 }
