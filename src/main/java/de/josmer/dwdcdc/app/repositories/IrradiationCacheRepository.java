b20d7fcb312e13d1e394baacedb05a2b78be4bf5
josmer <julian.osmer@outlook.de>
2019-07-03 14:52:27 +0200

    changes

--- src/main/java/de/josmer/dwdcdc/app/repositories/IrradiationCacheRepository.java
+++ src/main/java/de/josmer/dwdcdc/app/repositories/IrradiationCacheRepository.java
@@ -68,7 +68,7 @@ public class IrradiationCacheRepository extends Repository<IIrradiationCache> im
 
     private String getSaveQuery(IIrradiationCache irradiationCache) {
         return "INSERT INTO irradiation (id, db_cache, created) VALUES (" + irradiationCache.getId() + ",'"
-                + getDbCache(irradiationCache) + "', '" + Timestamp.valueOf(LocalDateTime.now()) + "';";
+                + getDbCache(irradiationCache) + "', '" + Timestamp.valueOf(LocalDateTime.now()) + "');";
     }
 
     private String getFindQuery(String key) {
